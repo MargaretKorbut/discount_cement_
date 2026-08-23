bc5bfbf1885cfc5678780b8917dd62d72c55345d
Маргарита <margaritatemlakova@gmail.com>
2026-08-23 17:04:48 +0300

    Order: добавлена проверка на отрицательные числа

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -3,4 +3,9 @@ package org.example;
 import java.time.LocalDateTime;
 
 public record Order(LocalDateTime dateTime, String companyName, double kg) {
+    public Order {
+        if (kg < 0) {
+            throw new IllegalArgumentException("Количество кг не может быть отрицательным: " + kg);
+        }
+    }
 }
